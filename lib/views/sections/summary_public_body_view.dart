7081c228c57c79d766cf1096d5d2388db9d5b4c4
AaronCS25 <aaron.camacho@utec.edu.pe0>
2023-06-23 02:19:42 -0500

    todo ok

--- lib/views/sections/summary_public_body_view.dart
+++ lib/views/sections/summary_public_body_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:que_dijo_app/apis/summary_api_service.dart';
 import 'package:que_dijo_app/models/summary_get_model.dart';
-import 'package:que_dijo_app/widgets/summary_card_list.dart';
 import 'package:que_dijo_app/widgets/summary_card_public_list.dart';
 
 class BuildPublicSummaryView extends StatefulWidget {
